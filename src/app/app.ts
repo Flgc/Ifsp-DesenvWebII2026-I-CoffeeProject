942029f0e5b409f787a96683be775d65808cf5cf
Fábio Luís <ml@fabiotecnico1.biz>
2026-05-15 21:08:03 -0300

    Implementing Reactivity, Interface, and Signal in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,45 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
+
 export class App {
-  protected readonly title = signal('Projeto');
-  disciplina = "Desenvolvimento para internet II";
-  docente = "Profa. Adriana Belon "  
-  nome = 'Fábio Luís';  
+  page = signal('products');
+  products: Product[] = [
+    { id: '1', name: 'Coffee 1', description: 'Extra-strong type' },
+    { id: '2', name: 'Coffee 2', description: 'Flavoured type' },
+    { id: '3', name: 'Coffee 3', description: 'Arabica type' },
+    { id: '4', name: 'Coffee 4', description: 'Gourmet type' },
+    { id: '5', name: 'Coffee 3', description: 'Carioca type' },
+    { id: '6', name: 'Coffee 3', description: 'Decaffeinated type' },
+
+  ];
+
+  cart: Product[] = [];
+
+  addCart(product: Product) {
+    this.cart.push(product);
+  };
+
+  removeCart(index: number) {
+    this.cart.splice(index, 1);
+  };
+
+  goTo(next: string) {
+    this.page.set(next);
+  }
 }
